9e128eb88052ec3fc2f0ad8dd9248b66ceb77127
nibuhs <alexsandr.shubin.is21b@mail.ru>
2025-02-26 00:41:59 +0300

    Find function to compressing images

---
+++ logic/Сompression.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ProjectOffice.logic
+{
+    public class Сompression
+    {
+        public static byte[] CompressImage(string srcPath, int quality)
+        {
+            var FileName = Path.GetFileName(srcPath);
+            using (Bitmap bmp1 = new Bitmap(srcPath))
+            {
+                // check extension
+                ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+                System.Drawing.Imaging.Encoder QualityEncoder = System.Drawing.Imaging.Encoder.Quality;
+                EncoderParameters myEncoderParameters = new EncoderParameters(1);
+                EncoderParameter myEncoderParameter = new EncoderParameter(QualityEncoder, quality);
+                myEncoderParameters.Param[0] = myEncoderParameter;
+                using (var ms = new MemoryStream())
+                {
+                    bmp1.Save(ms, bmp1.RawFormat);
+                    return ms.ToArray();
+                }
+            }
+        }
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+    }
+}
